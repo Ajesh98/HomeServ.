08fb72b265c4e6bf9d8cb6ee4031046d77306f4b
Ajesh98 <56129001+Ajesh98@users.noreply.github.com>
2020-02-22 11:04:33 +0530

    Add files via upload

---
+++ Homeserv.php
@@ -0,0 +1,164 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>HomeServ - Your perfect home solutions</title>
+	
+
+ 
+
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,400" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Simple Line Icons -->
+	<link rel="stylesheet" href="css/simple-line-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Style -->
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+	<header role="banner" id="fh5co-header">
+		<div class="container">
+			<div class="row">
+				<nav class="navbar navbar-default navbar-fixed-top">
+					<div class="navbar-header">
+						<!-- Mobile Toggle Menu Button -->
+						<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
+						<a class="navbar-brand" href="index.html">HomeServ.</a> 
+					</div>
+					<div id="navbar" class="navbar-collapse collapse">
+						<ul class="nav navbar-nav navbar-right">
+							<li class="active"><a href="#" data-nav-section="home"><span>Home</span></a></li>
+							<li><a href="#" data-nav-section="services"><span>About</span></a></li>
+							<li><a href="#" data-nav-section="explore"><span>Our Services</span></a></li>
+							<li><a href="#" data-nav-section="testimony"><span>Book Complaint</span></a></li>
+						</ul>
+					</div>
+					<a id="Signup" href="signup.php" data-nav-section="blog"><span>Register</span></a>
+					<a id="Login" href="login.php" data-nav-section="pricing"><span>Login</span></a>
+				</nav>
+			</div>
+	  </div>
+	</header>
+
+
+
+	<section id="fh5co-home" data-section="home" style="background-image: url(images/homeserv.jpg);" data-stellar-background-ratio="0.5">
+		<div class="gradient"></div>
+		<div class="container">
+			<div class="text-wrap">
+				<div class="text-inner">
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2 text-center">
+							<h1 class="animate-box"><span class="big">HomeServ</span> <br><span>Your Perfect Home Solution</span> </h1>
+							
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="fh5co-services" data-section="services">
+		<div class="fh5co-services">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 text-center">
+						<div class="box-services">
+							<img src="images/images.jpg" width="20%" height="10%" border-radius: 50%>
+							<div class="fh5co-post animate-box">
+								<h3>Plumber</h3>
+								<p>Taps, Tanks, Shower, Sanitaryware and more</p>
+							</div>
+						</div>
+
+						<div class="box-services">
+							<div class="icon animate-box">
+								<img src="images/mecha.jpg" width="35%" height="10%" border-radius: 50%>
+							</div>
+							<div class="fh5co-post animate-box">
+								<h3>Mechanical</h3>
+								<p>Cars, Bikes, and other Repairs</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 text-center">
+						<div class="box-services">
+							<img src="images/elec.jpg" width="25%" height="10%" border-radius: 50%>
+							<div class="fh5co-post animate-box">
+								<h3>Electrician</h3>
+								<p>Fans, Lights, Refrigerator and more</p>
+							</div>
+						</div>
+
+						<div class="box-services">
+							<div class="icon animate-box">
+								<img src="images/carpen.jpg" width="35%" height="10%" border-radius: 50%>
+							</div>
+							<div class="fh5co-post animate-box">
+								<h3>Carpenter</h3>
+								<p>Doors, Windows, Furniture and more</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 text-center">
+						<div class="box-services">
+							<img src="images/ac.jpg" width="35%" height="10%" border-radius: 50%>
+							<div class="icon animate-box">
+								
+							</div>
+							<div class="fh5co-post animate-box">
+								<h3>AC & Refrigerator</h3>
+								<p>AC Installation, Servicing, Gas Filling and more</p>
+							</div>
+						</div>
+					</div>
+						
+	</section>
+
+	
+
+
+
+	
+
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
